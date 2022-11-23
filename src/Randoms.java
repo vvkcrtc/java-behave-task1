582e79dd54ca5a8616c78f46c7d953ff7a3e99eb
vvkcrtc <105951181+vvkcrtc@users.noreply.github.com>
2022-11-23 17:12:33 +0300

    Add files via upload

---
+++ src/Randoms.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min;
+    int max;
+    int rand = 0;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                random = new Random();
+                rand = random.nextInt(min, max + 1);
+                return true;
+
+            }
+
+            @Override
+            public Integer next() {
+                return rand;
+            }
+        };
+    }
+}
